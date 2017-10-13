6551a74b227935e8f7568adb80a93e23ee0096c3
Crista Cooper <aine3d@gmail.com>
2017-10-13 20:30:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,12 +29,13 @@ function viewCart() {
       var price = cart[i][item];
       if( cart.length === 1) {
       console.log("In your cart, you have " + item + " at $" + price + ".")
-    } else if (cart.length === 2) { 
+    } else if (cart.length === 2) {
       for (var i = 0; i < cart.length; i++) {
       console.log("In your cart, you have " + item[i] + " at $" + price[i] + and )
     }
     }
   }
+}
 }
 }
 
